f810e3e8a397a441080a6985e54f1b65eeacb4fa
Kobe <dev.skyachieve91@gmail.com>
2024-04-17 07:42:41 +0900

    :sparkles: Feat: '헤더, 대표 이미지 및 팀 소개 구현'

---
+++ script.js
@@ -0,0 +1,37 @@
+/* 수직 스크롤이 발생하면 header 태그에 active 클래스 추가 및 삭제 */
+/* scroll_request.js */
+const headerEl = document.querySelector("header");
+window.addEventListener('scroll', function(){
+  this.requestAnimationFrame(scrollCheck);
+})
+function scrollCheck() {
+  let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+  if(browerScrollY > 0){
+    headerEl.classList.add("active");
+  }else{
+    headerEl.classList.remove("active");
+  }
+}
+/* end scroll_request.js */
+
+/* scroll.js */
+/* 애니메이션 스크롤 이동 */
+const animationMove = function(selector){
+  // ① selector 매개변로 이동할 대상 요소 노드 가져오기
+  const targetEl = document.querySelector(selector);
+  // ② 현재 브라우저의 스크롤 정보(y 값)
+  const browserScrollY = window.pageYOffset;
+  // ③ 이동할 대상의 위치(y 값)
+  const targetScorllY = targetEl.getBoundingClientRect().top + browserScrollY;
+  // ④ 스크롤 이동
+  window.scrollTo({ top: targetScorllY, behavior: 'smooth' });
+};
+// 스크롤 이벤트 연결하기
+const scollMoveEl = document.querySelectorAll("[data-animation-scroll='true']"); 
+for(let i = 0; i < scollMoveEl.length; i++){
+  scollMoveEl[i].addEventListener('click', function(e){
+    const target = this.dataset.target;
+    animationMove(target);
+  });
+}
+/* End scroll.js */
